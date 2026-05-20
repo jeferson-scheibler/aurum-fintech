7277c27e86b6184ae24c08f267bafed6ede39740
Jeferson Scheibler <jeferson.scheibler@universo.univates.br>
2026-05-19 21:44:42 -0300

    fix: deploy sempre derruba containers antes de subir e jenkins puxa codigo antes de deployar

--- deploy-homolog.sh
+++ deploy-homolog.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
+docker compose -f docker-compose.homolog.yml down
 docker compose -f docker-compose.homolog.yml up -d --build
 echo "Homolog atualizado: http://177.44.248.105:8081"
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
+docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
 echo "Prod atualizado: http://177.44.248.105:8082"
